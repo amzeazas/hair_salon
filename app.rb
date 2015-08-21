51a896bfb4c56fbc3048020d9dd6c33d4e4931eb
Alexa <amzeazas@comcast.net>
2015-08-21 12:29:42 -0700

    Add stylists.erb page with passed integration spec.

--- app.rb
+++ app.rb
@@ -12,3 +12,14 @@ DB = PG.connect({:dbname => "hair_salon_test"})
 get('/') do
   erb(:index)
 end
+
+get('/stylists') do
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+post('/stylists') do
+  stylist = Stylist.new({:first_name => params.fetch("first_name"), :last_name => params.fetch("last_name"), :phone_number => params.fetch("phone_number")})
+  stylist.save()
+  redirect('/stylists')
+end
