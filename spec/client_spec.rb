098ec1df96806a7eb69d50c3efc754d4cc2d5466
Alexa <amzeazas@comcast.net>
2015-08-21 08:25:44 -0700

    Add spec for phone_number method to Client class.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -13,4 +13,10 @@ describe(Client) do
       expect(test_client.last_name()).to(eq("Wayne"))
     end
   end
+  describe('#phone_number') do
+    it('returns the phone number of the client') do
+      test_client = Client.new({:first_name => "Bruce", :last_name => "Wayne", :phone_number => "5038675309", :id => nil})
+      expect(test_client.phone_number()).to(eq("5038675309"))
+    end
+  end
 end
