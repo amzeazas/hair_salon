cd59a945bcaf3dde48c3e156b85381734e31d7be
Alexa <amzeazas@comcast.net>
2015-08-21 08:30:05 -0700

    Add .all method to Client class.

--- lib/client.rb
+++ lib/client.rb
@@ -7,4 +7,17 @@ class Client
     @phone_number = attr.fetch(:phone_number)
     @id = attr.fetch(:id, nil)
   end
+
+  define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each() do |client|
+      first_name = client.fetch("first_name")
+      last_name = client.fetch("last_name")
+      phone_number = client.fetch("phone_number")
+      id = client.fetch("id").to_i()
+      clients.push(Client.new({:first_name => first_name, :last_name => last_name, :phone_number => phone_number, :id => id}))
+    end
+    clients
+  end
 end
