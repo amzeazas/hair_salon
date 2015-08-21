823016ae44559f637e61f1fce8ded71cdddad407
Alexa <amzeazas@comcast.net>
2015-08-21 08:32:08 -0700

    Add spec for save method to Client class.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -24,4 +24,11 @@ describe(Client) do
       expect(Client.all()).to(eq([]))
     end
   end
+  descirbe('#save') do
+    it('lets you save a client into the database') do
+      client = Client.new({:first_name => "Bruce", :last_name => "Wayne", :phone_number => "5038675309", :id => nil})
+      client.save()
+      expect(Client.all()).to(eq([client]))
+    end
+  end
 end
